729531bc497a8ce23f9a38c925b7b1f0f1847ae0
Lucas Pope <lpopedv@proton.me>
2025-04-10 23:14:40 -0300

    fix: crudOperations in delegate fn's

--- lib/x_finances/transactions.ex
+++ lib/x_finances/transactions.ex
@@ -1,13 +1,9 @@
 defmodule XFinances.Transactions do
-  alias XFinances.Transactions.Create
-  alias XFinances.Transactions.Update
-  alias XFinances.Transactions.Delete
-  alias XFinances.Transactions.List
-  alias XFinances.Transactions.Show
+  alias XFinances.Transactions.CrudOperations
 
-  defdelegate create(params), to: Create, as: :call
-  defdelegate update(params), to: Update, as: :call
-  defdelegate delete(id), to: Delete, as: :call
-  defdelegate list(), to: List, as: :call
-  defdelegate show(id), to: Show, as: :call
+  defdelegate create(params), to: CrudOperations
+  defdelegate update(transaction, params), to: CrudOperations
+  defdelegate delete(id), to: CrudOperations
+  defdelegate list(), to: CrudOperations
+  defdelegate show(id), to: CrudOperations
 end
