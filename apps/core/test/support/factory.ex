b7b8b459d779b452ea0165255d7ff171a618f711
Lucas Pope <lpopedv@proton.me>
2025-04-13 19:07:16 -0300

    feat: update factories

--- apps/core/test/support/factory.ex
+++ apps/core/test/support/factory.ex
@@ -2,11 +2,36 @@ defmodule Core.Factory do
   use ExMachina.Ecto, repo: Core.Repo
 
   def user_factory do
-    %{
+    password = "password123"
+
+    %Core.Schemas.User{
       full_name: "User Name",
       email: sequence(:email, &"user#{&1}@email.com"),
-      password: "password123",
+      password: password,
+      password_hash: Argon2.hash_pwd_salt(password),
       base_income: 0
+    }
+  end
+
+  def category_factory do
+    %Core.Schemas.Category{
+      user: build(:user),
+      title: sequence(:title, &"Category #{&1}"),
+      description: "Category for transactions"
+    }
+  end
+
+  def transaction_factory do
+    %Core.Schemas.Transaction{
+      user: build(:user),
+      category: build(:category),
+      title: "Transaction 01",
+      movement: :outgoing,
+      value_in_cents: 340_000,
+      date: Date.utc_today(),
+      due_date: nil,
+      is_fixed: true,
+      is_paid: false
     }
   end
 end
