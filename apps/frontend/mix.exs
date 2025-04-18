48c87b7d131b1e175f66f54f0b1a785fe62f9739
Lucas Pope <lpopedv@proton.me>
2025-04-18 10:39:22 -0300

    feat: add core in frontend project

--- apps/frontend/mix.exs
+++ apps/frontend/mix.exs
@@ -55,7 +55,8 @@ defmodule Frontend.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:core, in_umbrella: true}
     ]
   end
 
